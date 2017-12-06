dd039b8f8bf1518f6056d62e52375ead77bc5636
MatthewAHarvey <matthew.a.harvey@gmail.com>
2017-12-06 14:49:02 +0000

    Add files via upload

---
+++ MicroTimerExample.ino
@@ -0,0 +1,29 @@
+#include "MicroTimer.h"
+
+long halfSecond = 5e5;
+MicroTimer eventTimer;
+MicroTimer tenSecondTimer(10e6);
+MicroTimer totalTimer;
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.println("Connected to MicroTimer test.");
+    
+    eventTimer.init(halfSecond);
+    tenSecondTimer.reset();
+    totalTimer.reset();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    if(eventTimer.timedOut(true))
+    { 
+        Serial.print("Half second elapsed: ");
+        Serial.println(totalTimer.elapsed());
+    }
+    if(tenSecondTimer.timedOut(false))
+    {
+        Serial.println("Tens seconds elapsed.");
+    }
+}
